90b460e7dcc1e1bb67f69e8706aeaeba67677401
Roman PETER <grafik.r.peter@gmail.com>
2018-10-01 12:19:30 +0200

    intermediate excercise

--- int1/myscript.js
+++ int1/myscript.js
@@ -2,7 +2,20 @@ $("form").submit(function(e) {
 
 	e.preventDefault();
 	
-	
-	
-
 });
+
+// $("form").remove();
+// removed form
+
+var newDiv = $('<div></div><div></div><div></div><div></div><div></div>');
+$("article").prepend(newDiv);
+
+$("article div").addClass("box");
+
+$("a").attr('href', 'http://www.codefactory.wien');
+
+
+$("#msg").val("Search for..."); 
+
+
+$()
